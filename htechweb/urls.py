3636fe064d78d01e78fd5d08d5492cc9e087e642
Connor Cash <cashconnor@protonmail.com>
2022-01-15 16:52:18 -0500

    view and url update for landing page

--- htechweb/urls.py
+++ htechweb/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import LandingPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', LandingPageView.as_view()),
 ]
--- htechweb/views.py
+++ htechweb/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.views import generic
+from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("Homepage")
+
+class LandingPageView(generic.TemplateView):
+    template_name = "index.html"
+
+
+def landing_page(request):
+    return render(request, "landing.html")
