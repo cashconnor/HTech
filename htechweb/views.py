c9b55c074725336966820e82f91e9b6dbb78add8
Connor Cash <cashconnor@protonmail.com>
2022-01-17 17:15:00 -0500

    tailwindcss setup, static files setup

--- htechweb/views.py
+++ htechweb/views.py
@@ -5,7 +5,3 @@ from django.shortcuts import render
 
 class LandingPageView(generic.TemplateView):
     template_name = "index.html"
-
-
-def landing_page(request):
-    return render(request, "landing.html")
