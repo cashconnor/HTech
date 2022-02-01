e31c56b8c4b270a8cb307ad751705d57826ffd8c
Connor Cash <cashconnor@protonmail.com>
2022-02-01 15:35:43 -0500

    minor fixes

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -28,3 +28,13 @@ methods: {
         }
     }
 }
+
+$(window).scroll(function() {     
+    var scroll = $(window).scrollTop();
+    if (scroll > 0) {
+        $("#header").addClass("active");
+    }
+    else {
+        $("#header").removeClass("active");
+    }
+});
