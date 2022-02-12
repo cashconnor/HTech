d45d718707453000f0563a7f884f409a681ed69a
Connor Cash <cashconnor@protonmail.com>
2022-02-12 17:41:32 -0500

    static root update

--- htechweb/settings.py
+++ htechweb/settings.py
@@ -144,7 +144,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-STATIC_ROOT = "static_root"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
