9870af4c85dc8c538b2fd8a3f67c2876fd4a36b3
Connor Cash <cashconnor@protonmail.com>
2022-02-12 17:39:12 -0500

    settings file debug and logging

--- htechweb/settings.py
+++ htechweb/settings.py
@@ -161,5 +161,40 @@ DEFAULT_FROM_EMAIL = 'ccc262@cornell.edu'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())
