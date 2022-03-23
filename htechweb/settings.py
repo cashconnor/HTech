2e1616e606e1c63165514308cd9cc5450d9dc041
Connor Cash <cashconnor@protonmail.com>
2022-03-23 18:57:22 -0400

    debugging email sending

--- htechweb/settings.py
+++ htechweb/settings.py
@@ -159,13 +159,13 @@ CRISPY_TEMPLATE_PACK = "tailwind"
 
 DEFAULT_FROM_EMAIL = 'ccc262@cornell.edu'
 
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'connor.cash1@gmail.com'
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+EMAIL_USE_SSL = False
 
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
