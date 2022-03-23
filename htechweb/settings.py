281718d8ff524e481ebb81adf88cd9729bf9820b
Connor Cash <cashconnor@protonmail.com>
2022-03-23 18:59:56 -0400

    debugging email send

--- htechweb/settings.py
+++ htechweb/settings.py
@@ -164,7 +164,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'connor.cash1@gmail.com'
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_USE_SSL = False
 
 
